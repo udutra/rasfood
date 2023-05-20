7c50130c8b4ef5926a95f910d7471fd3fc81c69e
Guilherme Galarça Dutra <udutra@gmail.com>
2023-05-20 16:26:26 -0300

    Create Prato.java

---
+++ src/main/java/br/com/guilhermedutra/rasmoo/restaurante/entity/Prato.java
@@ -0,0 +1,87 @@
+package br.com.guilhermedutra.rasmoo.restaurante.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "pratos")
+public class Prato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+    private boolean disponivel;
+    private BigDecimal valor;
+
+    @Column(name = "data_de_registro")
+    private LocalDateTime dataDeRegistro = LocalDateTime.now();
+
+    public Prato() {}
+
+    public Prato(Long id, String nome, String descricao, boolean disponivel, BigDecimal valor) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.disponivel = disponivel;
+        this.valor = valor;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getDataDeRegistro() {
+        return dataDeRegistro;
+    }
+
+    public void setDataDeRegistro(LocalDateTime dataDeRegistro) {
+        this.dataDeRegistro = dataDeRegistro;
+    }
+
+    @Override
+    public String toString() {
+        return "Prato{ id:" + getId() + ", nome: " + getNome() + ", descricao: " + getDescricao() + ", disponivel: " + isDisponivel() +
+                ", valor: " + getValor() + ", dataDeRegistro: " + getDataDeRegistro() + "}";
+    }
+}
